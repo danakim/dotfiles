714f6ac952ebdcb7ba5b5df4e459d944d1293f51
Dan <dan@hostatic.ro>
2016-09-05 21:06:21 +0300

    Small change to where the files will be outputed

--- bootstrapme.sh
+++ bootstrapme.sh
@@ -5,17 +5,16 @@
 cd $HOME
 
 # Get an archive of our dotfiles from Github
-wget https://github.com/danakim/dotfiles/archive/master.tar.gz
-mkdir dotfiles
-tar -xvzf master.tar.gz -C ./dotfiles --strip-components=1
+wget https://github.com/danakim/dotfiles/archive/master.tar.gz && mkdir dotfiles
+tar -xvzf master.tar.gz -C ./.dotfiles --strip-components=1 && rm -f master.tar.gz
 
 # Create our dotfiles symlinks (and remove the original files)
 rm -f .bashrc .inputrc .gitconfig .vim .vimrc
-ln -s ~/dotfiles/bashrc_server .bashrc
-ln -s ~/dotfiles/inputrc .inputrc
-ln -s ~/dotfiles/vimrc .vimrc
-ln -s ~/dotfiles/vim .vim
-ln -s ~/dotfiles/gitconfig .gitconfig
+ln -s ~/.dotfiles/bashrc_server .bashrc
+ln -s ~/.dotfiles/inputrc .inputrc
+ln -s ~/.dotfiles/vimrc .vimrc
+ln -s ~/.dotfiles/vim .vim
+ln -s ~/.dotfiles/gitconfig .gitconfig
 
 # Drop our SSH key in (we probably need the .ssh dir first)
 mkdir .ssh && chmod 700 .ssh
