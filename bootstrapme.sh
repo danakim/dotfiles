34823b59747141be5f85fe0bcaf29c644a49b261
Dan <dan@hostatic.ro>
2016-09-07 11:37:47 +0300

    Making things a bit more resilient

--- bootstrapme.sh
+++ bootstrapme.sh
@@ -9,7 +9,7 @@ wget https://github.com/danakim/dotfiles/archive/master.tar.gz && mkdir .dotfile
 tar -xvzf master.tar.gz -C ./.dotfiles --strip-components=1 && rm -f master.tar.gz
 
 # Create our dotfiles symlinks (and remove the original files)
-rm -f .bashrc .inputrc .gitconfig .vim .vimrc
+rm -rf .bashrc .inputrc .gitconfig .vim .vimrc
 ln -s ~/.dotfiles/bashrc_server .bashrc
 ln -s ~/.dotfiles/inputrc .inputrc
 ln -s ~/.dotfiles/vimrc .vimrc
@@ -17,5 +17,7 @@ ln -s ~/.dotfiles/vim .vim
 ln -s ~/.dotfiles/gitconfig .gitconfig
 
 # Drop our SSH key in (we probably need the .ssh dir first)
-mkdir .ssh && chmod 700 .ssh
-ln -s ~/.dotfiles/id_dsa.pub .ssh/id_dsa.pub && chmod 600 .ssh/id_dsa.pub
+if [ ! -d ~/.ssh ] ; then
+    mkdir ~/.ssh && chmod 700 ~/.ssh
+fi
+ln -s ~/.dotfiles/id_dsa.pub ~/.ssh/id_dsa.pub && chmod 600 ~/.ssh/id_dsa.pub
